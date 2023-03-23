918be65d17ac9cb98cd35694bcd5883825c60dee
Ryan Dantzler <ryandantzler@gmail.com>
2023-03-22 23:06:33 -0700

    Added asset resource loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import generateJoke from './generateJoke';
 import './styles/main.scss'
+import tomato from './assets/tomato.svg';
+
+const tomatoImg = document.getElementById('tomatoImg');
+tomatoImg.src = tomato;
 
 console.log(generateJoke());
